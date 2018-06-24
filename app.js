4194399852522ffe6687251d744e17789dc9dc65
Emmanuel Sarpong <esarpong51@gmail.com>
2018-06-24 05:49:22 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 //console.log(util.format('%s:%s', 'foo'));
-
+const util = require('util');
+const request = require('request')
+const {WebhookClient} = require('dialogflow-fulfillment');
 const express = require('express');
 const bodyParser = require('body-parser');
-const http = require('https');
 
 const server = express();
 server.use(bodyParser.urlencoded({
@@ -12,51 +13,41 @@ server.use(bodyParser.urlencoded({
 
 server.use(bodyParser.json());
 
-server.post('/get-fx-rate', (req, res) => {
+server.post('/', (req, res) => {
 
-    if( !(req.body.queryResult && req.body.queryResult.parameters) ) {
-      return res.json({
-        err: "Error"
+  const agent = new WebhookClient({ req, res });
+
+    let intents = new Map();
+    intents.set('account.balance.check', fallback);
+    intents.set('currency.convert', testFx);
+    intents.set('account.spending.check', fallback);
+    intents.set('transfer.money', fallback);
+    agent.handleRequest(intents);
+
+    function testFx() {
+
+      const parameters = req.body.queryResult.parameters,
+        c_from = parameters["currency-from"] || 'USD',
+        c_to = parameters["currency-to"] || 'GHS',
+        amount = parameters["amount"] || 1;
+
+      request
+      .get(`https://www.amdoren.com//api/currency.php?api_key=8v3VvUXYeEGniBPuANKHbqxp5tRV2v&from=${c_from}&to=${c_to}`)
+      .on('response', function(response) {
+        const resp = JSON.parse(reponse.body.toString());
+        let value = `${resp.amount * amount}`;
+        agent.add(value);
       })
+      
     }
-    const parameters = req.body.queryResult.parameters,
-          c_from = parameters["currency-from"] || 'USD',
-          c_to = parameters["currency-to"] || 'GHS',
-          amount = parameters["amount"] || 1,
-          reqUrl = `https://www.amdoren.com//api/currency.php?api_key=8v3VvUXYeEGniBPuANKHbqxp5tRV2v&from=${c_from}&to=${c_to}`;
     
-    http.get(reqUrl, 
-      (t_res) => {
-          var chunks = [];
-
-          t_res.on("data", function (chunk) {
-            chunks.push(chunk);
-          });
-
-          t_res.on('end', () => {
-
-            var body = Buffer.concat(chunks);
-            console.log(body.toString())
-            const resp = JSON.parse(body.toString());
-            let dataToSend = `${resp.amount * amount}`;
-
-            return res.json({
-              speech: dataToSend,
-              displayText: dataToSend
-            });
-
-          });
-      }, 
-      (error) => {
-          return res.json({
-              speech: 'Something went wrong!',
-              displayText: 'Something went wrong!'
-          });
-      }
-    );
+    function fallback(agent) {
+      agent.add(`I'm sorry, api not hooked up yet.`);
+    }
 
 });
 
 server.listen((process.env.PORT || 8000), () => {
     console.log("Server is up and running...");
 });
+
