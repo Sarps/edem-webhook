7893f85c3a32699d25e853696a1b7b55890830b7
Emmanuel Sarpong <esarpong51@gmail.com>
2018-06-25 18:47:27 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ server.post('/', (req, res) => {
           if(err) return resolve (`There is no data available for convertion on ${variable} and ${fixed}`);
           var arr = result.GetFXRateResult.split("=");
           if(arr && arr.length > 1) {
-            var date = arr[0].match(/\d+\/\d+\/\d+\s+\d+:\d+:\d+ \w+/g)[0];
+            var date = arr[0].match(/\d+\/\d+\/\d/g)[0];
             date = moment(date, "DD-MM-YYYY");
             return resolve (
               util.format(req.body.queryResult.fulfillmentText, date, (arr[1] * amount).toFixed(2))
@@ -80,7 +80,7 @@ server.post('/', (req, res) => {
           if(err) return resolve (`There is no data available for convertion on ${variable} and ${fixed}`);
           var arr = result.GetFXRateResult.split("=");
           if(arr && arr.length > 1) {
-            var date = arr[0].match(/\d+\/\d+\/\d+\s+\d+:\d+:\d+ \w+/g)[0];
+            var date = arr[0].match(/\d+\/\d+\/\d+/g)[0];
             date = moment(date, "DD-MM-YYYY");
             resolve (
               util.format(req.body.queryResult.fulfillmentText, date, (1/arr[1] * amount).toFixed(2))
